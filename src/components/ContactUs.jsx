416eb2243870d1631c621de2af970c6bca7e240b
Priyal208 <priyalrwl.20@gmail.com>
2024-01-13 22:34:58 +0530

    Testimonials,Contact Us, Our App

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import i from '../imgs/Screenshot 2024-01-13 190835.png'
+
+const ContactUs = () => {
+    const handleButtonClick = () => {
+      window.location.href = 'https://www.youtube.com/@ronakinstitute1990';
+    };
+  return (
+    <div className='flex flex-row m-10'>
+    <div className='w-[40%] flex flex-col' data-aos="fade-right">
+      <div>
+        <p className='text-5xl md:text-2xl text-center font-bold ' >CONTACT US</p>
+        <p className='py-10 text-center p-5 text-4xl md:text-xl'>
+          FILL THE FORM TO GET FREE COUNSELLING SESSION 
+        </p>
+        <div className=' text-center'>
+          <button
+            className=' text-3xl md:text-xl sm:text-xs border-spacing-2 border-4 border-gray-900 btn2 p-2'
+            onClick={handleButtonClick}
+          >
+            CLICK HERE!
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div className='w-[60%] ml-60 md:ml-10' data-aos="fade-left">
+      <img src={i} alt='App Screenshot' className='h-[400px]' />
+    </div>
+  </div>
+  )
+}
+
+export default ContactUs;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,14 +3,21 @@ import ImageCarousel from '../components/ImageCaraousel.jsx';
 import AboutUs from '../components/AboutUs.jsx';
 import Video from '../components/Video.jsx';
 import Facilities from '../components/Facilities.jsx';
+import OurApp from '../components/OurApp.jsx';
+import ContactUs from '../components/ContactUs.jsx';
+import Testimonials from '../components/Testimonials.jsx';
 // Homepage.jsx
 const Homepage = () => {
   return (
     <div className='body'>
       <ImageCarousel />
       <AboutUs />
+      <ContactUs />
       <Video />
       <Facilities />
+     <Testimonials />
+      <OurApp />
+      
     </div>
   );
 };
